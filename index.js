179e5febf7e3bde4af9525afa775c10844ce0f8c
JenniferKiesler <111549041+JenniferKiesler@users.noreply.github.com>
2022-11-14 21:34:51 -0600

    adds search, add, and update functions

--- index.js
+++ index.js
@@ -8,6 +8,104 @@ const connection = mysql.createConnection({
     database: "sakila"
 })
 
+const searchActors = async () => {
+    const answer = await inquirer.prompt ([
+        {
+            type: "input",
+            name: "last_name",
+            message: "Last name:",
+        }
+    ])
+
+    try {
+        const [results] = await connection.promise().query(
+            'SELECT * FROM actor WHERE last_name = ?', 
+            [answer.last_name]
+        )
+        
+        console.table(results)
+    
+        menuPrompt()
+
+    } catch(err) {
+        throw new Error(err)
+    }
+
+}
+
+const addActor = async () => {
+    const answers = await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'first_name',
+            message: 'What is their first name?'
+        },
+        {
+            type: 'input',
+            name: 'last_name',
+            message: 'What is their last name?'
+        }
+    ])
+
+    try {
+        const [results] = await connection.promise().query(
+            `INSERT INTO actor (first_name, last_name)
+            VALUES (?, ?)`, [answers.first_name, answers.last_name]
+            )
+    
+        console.log('Actor added')
+    
+        menuPrompt()
+        
+    } catch(err) {
+        throw new Error(err)
+    }
+
+}
+
+const updateActor = async () => {
+    const answers = await inquirer.prompt([
+        {
+            type: 'number',
+            name: 'actor_id',
+            message: 'Which actor do you want to update? (ID)'
+        },
+        {
+            type: 'input',
+            name: 'first_name',
+            message: 'Update first name',
+            default: async (sessionAnswers) => {
+                const [results] = await connection.promise().query(
+                    'SELECT first_name FROM actor WHERE actor_id = ?', sessionAnswers.actor_id
+                )
+                return results[0].first_name
+            }
+        },
+        {
+            type: 'input',
+            name: 'last_name',
+            message: 'Update last name',
+            default: async (sessionAnswers) => {
+                const [results] = await connection.promise().query(
+                    'SELECT last_name FROM actor WHERE actor_id = ?', sessionAnswers.actor_id
+                )
+                return results[0].last_name
+            }
+        },
+    ])
+
+    const [results] = await connection.promise().query(
+        `UPDATE actor
+         SET first_name = ?, last_name = ?
+         WHERE actor_id = ?`, 
+        [answers.first_name, answers.last_name, answers.actor_id]
+    )
+
+    console.log('Actor updated!')
+
+    menuPrompt()
+}
+
 const menuPrompt = async () => {
    const answers = await inquirer.prompt([
         {
