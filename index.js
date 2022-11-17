33c7a7df4ea44b9a13df575461ba68c1131b504d
JenniferKiesler <111549041+JenniferKiesler@users.noreply.github.com>
2022-11-16 23:15:44 -0600

    adds .env file

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
+import * as dotenv from 'dotenv'
+dotenv.config()
 import mysql from 'mysql2'
 import inquirer from 'inquirer'
 
 const connection = mysql.createConnection({
     host: "localhost",
-    user: "root",
-    password: "root",
-    database: "sakila"
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 })
 
 const searchActors = async () => {
